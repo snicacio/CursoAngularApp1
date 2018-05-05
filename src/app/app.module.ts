ad785f617414df17b0c24e57439de746fcf2434c
Sandro Nicacio <nicaciodeveloper@gmail.com>
2018-05-04 23:00:32 -0300

    formatação do código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {TopoComponent} from './topo/topo.component';
+import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component'
 
 @NgModule({
